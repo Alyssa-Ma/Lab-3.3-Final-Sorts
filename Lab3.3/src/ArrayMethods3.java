c9434eb490ba572f3cafa0c02e2d86df565c561e
BT_1N3_32 <BT_1N3_32@BT_1N3_32.bktech.local>
2018-02-14 09:21:56 -0500

    aaa

--- Lab3.3/src/ArrayMethods3.java
+++ Lab3.3/src/ArrayMethods3.java
@@ -16,7 +16,65 @@ public class ArrayMethods3
 	}
 	public static void quickSort(int [] list1, int front, int back)
 	{
+		//base case front >= back
 		
+		if(back > front)
+		{
+			/**
+			 * old partition method, needs to be updated
+			 * {
+		int x = 0;
+		int y = list.length - 1;
+		int z = 0;
+		int aa = 0;
+		
+		while(x != y)
+		{
+			//two sides of array
+			if(list[x] > list[y] && (x < y))
+			{
+				swap(list, x, y);
+				
+				z = x;
+				x = y;
+				y = z + 1;
+			}
+			else if((list[x] < list[y]) && (x > y))
+			{
+				swap(list, x, y);
+				
+				z = x;
+				x = y;
+				y = z - 1;
+			}
+			else if(x > y)
+			{
+				y++;
+			}
+			else if(x < y)
+			{
+				y--;
+			}
+		}
+		return x;
+	}
+	
+	//helper method to swap items
+	public static void swap(int[] list, int in, int in2)
+	{
+		int n = list[in];
+		list[in] = list[in2];
+		list[in2] = n;
+	}
+			 */
+			int piv = partition(list1, front, back);
+			
+			//left side
+			quickSort(list1, front, piv-1);
+			
+			//right side
+			quickSort(list1, piv+1, back);
+		}
 	}
 	
 	
