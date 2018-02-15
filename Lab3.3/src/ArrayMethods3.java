ae0702c33d0a629e1c692207d81760e06382ad9b
BT_1N3_32 <BT_1N3_32@BT_1N3_32.bktech.local>
2018-02-15 09:20:24 -0500

    tired

--- Lab3.3/src/ArrayMethods3.java
+++ Lab3.3/src/ArrayMethods3.java
@@ -1,18 +1,43 @@
+/**
+ * @author Alyssa Ma
+ * Period 2
+ * 2/15/18
+ * Lab 3.3
+ */
 
 public class ArrayMethods3 
 {
 	public static void main(String [] args)
 	{
+		//Testing arrays
+		String [] test1 = {"fishy", "kitty", "daughter", "no", "yes", "foo", "bar", "baz"};
+		int [] test3 = {};
 		
+		//Merge Sort
+		long start = System.nanoTime();
+		String[] mergeSortResult = mergeSort(test1);
+		long end = System.nanoTime();
+		long time = end - start;
+		System.out.println("Merge sort time: " + time + " nanoseconds.");
+		System.out.println(Arrays.toString(mergeSortResult));
+		
+		//Partition
+		start = System.nanoTime();
+		int = 
+		//Quick Sort
 	}
 	public static String[] mergeSort(String [] list)
 	{
 		//use copyOfRange to copy the array
+		//split the array and merge sort both sides, when both halves are length 1, merge
+		//keep dividing array into half
+		int start;
+		int end;
 		
 	}
 	public static int partition(int list1, int front, int back)
 	{
-		
+		//return position of pivot
 	}
 	public static void quickSort(int [] list1, int front, int back)
 	{
